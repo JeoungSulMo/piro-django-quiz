02540939add00d263b69fb0a19244d36d7062bc0
김지윤 <kimjiyoon1006@hanmail.net>
2019-07-24 16:54:35 +0900

    first jiyun commit

---
+++ quizGong/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\piro-django-quiz-master\piro-django-quiz-master\quizgong\quizgong\quizgong\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
